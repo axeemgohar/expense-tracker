06e8811eb275098f74117050ce09a34656ca36a4
Azeem Gohar <axeemgohar20@gmail.com>
2023-07-14 19:53:52 +0500

    Static data removed,Also filters won't show if there is no expense

--- src/App.js
+++ src/App.js
@@ -3,41 +3,14 @@ import AddExpense from "./components/addexpense/AddExpense.component";
 import Card from "./components/UI wrappers/Card.component";
 import { useState } from "react";
 
-const expense = [
-  {
-    id: 1,
-    title: "House Rent",
-    date: new Date(2022, 10, 5),
-    amount: 200,
-  },
-  {
-    id: 2,
-    title: "Car Wash",
-    date: new Date(2021, 11, 3),
-    amount: 30,
-  },
-  {
-    id: 3,
-    title: "Grocery",
-    date: new Date(2020, 8, 4),
-    amount: 50,
-  },
-  {
-    id: 4,
-    title: "Utility Bills",
-    date: new Date(2022, 7, 6),
-    amount: 100,
-  },
-];
 const App = () => {
-  const [userData, setUserData] = useState(expense);
+  const [userData, setUserData] = useState([]);
 
   const getUserInput = (value) => {
     setUserData((prevValue) => {
       return [value, ...prevValue];
     });
   };
-  console.log(userData);
 
   return (
     <>
